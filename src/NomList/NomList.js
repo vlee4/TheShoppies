44337d4514bc66579e59d0d3507ad4bb7c196d0b
Victoria <vicglee1234007@gmail.com>
2020-08-29 22:27:40 -0500

    Created mobile-friendly css

--- src/NomList/NomList.js
+++ src/NomList/NomList.js
@@ -1,7 +1,40 @@
 import React from "react";
 
 function NomList() {
-  return <div className="nomList">This is the Nom list</div>;
+  return (
+    <div className="nomList">
+      <div>This is the Nom List</div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        interdum sit amet dolor eu ullamcorper. Cras malesuada magna nisl, non
+        maximus metus rhoncus quis. Suspendisse venenatis urna libero, porta
+        consequat orci rutrum nec. Sed libero sapien, rhoncus non consequat sit
+        amet, dictum sit amet turpis. Nunc rutrum justo vitae pretium tristique.
+        Sed nec convallis neque. Proin suscipit faucibus commodo. Fusce a
+        pulvinar augue. Nunc et mollis enim, eu dictum diam. Pellentesque
+        habitant morbi tristique senectus et netus et malesuada fames ac turpis
+        egestas. Vivamus at odio et dui consectetur porttitor malesuada quis
+        ante. Nullam sit amet vestibulum ex. Maecenas laoreet mi ut nibh
+        malesuada, eget iaculis massa egestas. Pellentesque habitant morbi
+        tristique senectus et netus et malesuada fames ac turpis egestas. Donec
+        consectetur ipsum at nisl placerat volutpat. Pellentesque lobortis, nisl
+        quis aliquam cursus, sem sem ultricies nibh, ut cursus sapien mauris sed
+        nibh. Vivamus mollis mauris aliquam libero commodo, eu aliquet enim
+        consequat. Ut ac arcu a sem rutrum varius ac eget enim. Etiam tristique,
+        felis id accumsan dapibus, magna lacus ultrices velit, a rutrum velit
+        leo vel sem. Fusce nec porttitor neque. Vestibulum sit amet fringilla
+        diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+        posuere cubilia curae; Etiam elit metus, sollicitudin id sodales
+        ultrices, iaculis vel est. Ut congue metus sed nunc lacinia semper.
+        Vivamus ac lacus interdum, pellentesque diam a, egestas augue.
+        Pellentesque a leo quam. Maecenas vitae nunc eget felis scelerisque
+        convallis ac eu sapien. Aliquam ullamcorper blandit leo. Nulla tristique
+        neque sit amet tortor fringilla maximus. Donec vitae augue varius,
+        mollis quam at, mollis magna. Nunc tincidunt odio odio, in interdum urna
+        scelerisque vel. Sed maximus dignissim fringilla.
+      </p>
+    </div>
+  );
 }
 
 export default NomList;
--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 function Results() {
-  return <div className="results">Search Results go here!</div>;
+  return (
+    <div className="results">
+      <div>Search Results go here!</div>
+      <p>
+        Fusce tincidunt, diam nec semper sodales, dui ligula aliquet nulla, ut
+        bibendum lorem lorem vitae arcu. Integer sollicitudin sollicitudin elit,
+        et egestas felis placerat eget. Vestibulum egestas dui sit amet ipsum
+        tincidunt faucibus. Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Fusce eu elit at sapien tristique aliquam. Etiam est lacus,
+        euismod vel ultricies a, laoreet quis nisi. Suspendisse eget laoreet
+        arcu. Suspendisse potenti. Phasellus eleifend orci nec ex egestas,
+        viverra lobortis lorem ornare. Curabitur interdum risus et nisl
+        dignissim, vitae aliquam nisl sagittis. Proin rhoncus turpis nec
+        imperdiet lobortis. Vivamus vitae vulputate est. Nullam elementum dolor
+        enim, sit amet maximus dui ullamcorper a. Nulla ut velit sit amet turpis
+        luctus gravida. Maecenas sed rutrum sem. Nulla non faucibus ipsum.
+        Praesent rutrum suscipit tortor ut euismod. Cras eu lectus cursus,
+        venenatis nulla eu, elementum sapien. Nulla eu hendrerit libero.
+        Pellentesque ullamcorper mi sit amet lacus tincidunt, vel sagittis neque
+        eleifend. Duis dictum, lacus sit amet ornare volutpat, lectus enim
+        posuere nisi, sit amet tempor neque sapien elementum sapien. Sed in
+        dolor turpis.
+      </p>
+    </div>
+  );
 }
 
 export default Results;
