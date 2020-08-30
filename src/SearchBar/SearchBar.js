f4004ca5194b59723e6b7b8ea2778b0aef463d21
Victoria <vicglee1234007@gmail.com>
2020-08-30 02:38:53 -0500

    button-less search bar in progress

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,14 +1,47 @@
 import React from "react";
 
-function SearchBar() {
-  return (
-    <div className="SearchBar">
-      <form>
-        {/* <label htmlFor="search">Search for movies: </label> */}
-        <input type="search" placeholder="Search by movie title"></input>
-      </form>
-    </div>
-  );
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      query: "",
+      typing: false,
+    };
+    this.doSearch = this.doSearch.bind(this);
+    this.startSearch = this.startSearch.bind(this);
+  }
+  //onChange -> trigger timer -> query
+
+  doSearch() {
+    console.log(`Searching with: ${this.state.query}`);
+    //Make API call
+  }
+
+  startSearch(e) {
+    //if query is the same as x seconds ago, and typing is false, do search
+    //else just set new query value
+
+    //try debounce
+    this.setState({ query: e.target.value });
+    setTimeout(() => {
+      this.doSearch();
+    }, 3000);
+  }
+  render() {
+    return (
+      <div className="SearchBar">
+        <form>
+          {/* <label htmlFor="search">Search for movies: </label> */}
+          <input
+            type="search"
+            placeholder="Search by movie title"
+            value={this.state.query}
+            onChange={this.startSearch}
+          ></input>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
