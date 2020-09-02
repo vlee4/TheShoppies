327e95c6caf8b1b81ee16a441459695e7fe69dd2
Victoria <vicglee1234007@gmail.com>
2020-09-01 20:51:39 -0500

    searchbar can search make api data request

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { debounce } from "lodash";
+import { API_KEY } from "../secrets";
+import axios from "axios";
 
 class SearchBar extends React.Component {
   constructor() {
     super();
     this.state = {
       query: "",
+      results: {},
     };
     this.doSearch = this.doSearch.bind(this);
     this.startSearch = this.startSearch.bind(this);
   }
   //onChange -> trigger timer -> query
 
-  doSearch = debounce(() => {
+  doSearch = debounce(async () => {
     console.log(`Searching with: ${this.state.query}`);
-    //Make API call
+    let { data } = await axios.get(
+      `http://www.omdbapi.com/?s=${this.state.query}&apikey=${API_KEY}`
+    );
+    console.log("HERE's the DATA:", data);
   }, 3000);
 
   startSearch(e) {
     this.setState({ query: e.target.value });
     if (e.target.value.trim().length) {
-      console.log("value", e.target.value.trim());
+      // console.log("value", e.target.value.trim());
       this.doSearch();
     }
   }
