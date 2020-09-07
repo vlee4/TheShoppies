9514437899486e94d72b8a9baa3dda6a1190cdf0
Victoria <vicglee1234007@gmail.com>
2020-09-07 14:45:50 -0500

    Submit button appears after 5 noms added to list; Submitting will prevent further editing of nominations

--- src/Components/NomList.js
+++ src/Components/NomList.js
@@ -3,7 +3,30 @@ import SingleMovie from "./SingleMovie";
 import { connect } from "react-redux";
 
 class NomList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      submitted: false,
+    };
+    this.submitNominations = this.submitNominations.bind(this);
+  }
+
+  submitNominations() {
+    //Note: if db was connected, this.props.nominations could be sent to redux store and added to the db within a thunk
+    this.setState({ submitted: true });
+  }
+
   render() {
+    if (this.state.submitted) {
+      return (
+        <div className="nomList">
+          <div className="nomBanner">
+            Thank you for voting. Nominations have been submitted
+          </div>
+        </div>
+      );
+    }
+
     const { nominations } = this.props;
     // console.log("HERE are the Nominations:", nominations);
     const nomArr = [];
@@ -16,7 +39,7 @@ class NomList extends React.Component {
     return (
       <div className="nomList">
         <h4>Nominations List</h4>
-        <div id="nomBanner">
+        <div className="nomBanner">
           {nomArr.length === 5 ? "Nominations are full" : ""}
         </div>
         <div>
@@ -37,6 +60,11 @@ class NomList extends React.Component {
             <div>Select a movie to be added to the Nomination list</div>
           )}
         </div>
+        {nomArr.length === 5 ? (
+          <button onClick={this.submitNominations}>Submit</button>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
